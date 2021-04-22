24dbaba28d329f9a0cd69804e83bb0574dd4b0aa
Anupam kumar <[anupamit011@gmail.com]>
2021-04-22 23:15:02 +0530

    Update in js with sign up page

--- public/signup.js
+++ public/signup.js
@@ -5,7 +5,6 @@ async function signUp() {
   let emailElement = document.getElementById('email');
   let mobilenoElement = document.getElementById('mobileno');
   let pwElement = document.getElementById('password');
-  let rpwElement = document.getElementById('repassword');
   let roleElement = document.getElementById('role');
 
   let firstName = fnElement.value
@@ -13,13 +12,12 @@ async function signUp() {
   let email = emailElement.value
   let mobile = mobilenoElement.value
   let pwd = pwElement.value
-  let pwd2 = rpwElement.value
   let role = roleElement.value
-  console.log(firstName, lastName, email, mobile, pwd, pwd2, role);
+  console.log(firstName, lastName, email, mobile, pwd, role);
 
   let user = await firebase.auth().createUserWithEmailAndPassword(email,pwd)
   let uid = user.user.uid
-  await firebase.firestore().collection('users').doc(uid).set({firstName, lastName, email, mobile, pwd, pwd2, role})
+  await firebase.firestore().collection('users').doc(uid).set({firstName, lastName, email, mobile, pwd, role})
   console.log('signUp done', user);
   console.log('signUp uid', uid);
 }
